c4230aa615fbd19471fe50890d6e964fbd979cd0
Phạm Phong Phú Cường <51190165+phamcuong2751@users.noreply.github.com>
2020-04-16 22:24:57 +0700

    finish

--- index.js
+++ index.js
@@ -9,47 +9,140 @@ let readlineSync = require('readline-sync');
 let fs = require('fs');
 let allContacts = [];
 
-
-
 function loadData() {
-    let loadFile = fs.readFileSync('./data.json', { encoding: 'utf-8' });
+    let loadFile = fs.readFileSync('./data.json', { encoding: 'utf8' });
     allContacts = JSON.parse(loadFile);
 }
 
+function saveFile() {
+    let contentFile = JSON.stringify(allContacts);
+    fs.writeFileSync('./data.json', contentFile, { encoding: 'utf8' })
+}
+
+function addContact() {
+    let name = readlineSync.question('Name: ');
+    let phone = readlineSync.question('Phone: ');
+    let human = {
+        name: name,
+        phone: phone
+    }
+    allContacts.push(human);
+    saveFile();
+    console.log('<<< Additon contact successful!!');
+}
+
+function setContact() {
+    let findName = readlineSync.question('Name: ');
+    let findPhone = readlineSync.question('Phone: ');
+    let findHuman = {
+        name: findName,
+        phone: findPhone
+    }
+    let i = 1;
+    for (let human of allContacts) {
+        if (human.name == findHuman.name && human.phone == findHuman.phone) {
+            let newName = readlineSync.question('Enter new Name: ');
+            let newPhone = readlineSync.question('Enter new Phone: ');
+            let newHuman = {
+                name: newName,
+                phone: newPhone
+            }
+            human.name = newHuman.name;
+            human.phone = newHuman.phone;
+            break;
+        } else if (i === allContacts.length) {
+            console.log('<<< Not found!', findHuman.name, findHuman.phone);
+            break;
+        }
+        i++;
+    }
+    saveFile();
+}
+
+function removeContact() {
+    let findName = readlineSync.question('Name: ');
+    let findPhone = readlineSync.question('Phone: ');
+    let findHuman = {
+        name: findName,
+        phone: findPhone
+    }
+    for (let human of allContacts)
+        if (human.name == findHuman.name && human.phone == findHuman.phone) {
+            let index = allContacts.indexOf(human);
+            if (index > -1) {
+                allContacts.splice(index, 1);
+            }
+
+        }
+    saveFile();
+    console.log('<<< Successfully removed if exist phone or name');
+}
+
+function findContact() {
+    let findName = readlineSync.question('Name: ');
+    let findPhone = readlineSync.question('Phone: ');
+    let findHuman = {
+        name: findName,
+        phone: findPhone
+    }
+    for (let human of allContacts) {
+        if (human.name == findHuman.name && human.phone == findHuman.phone) {
+            console.log('<<< In the contact list, there are people you are looking for!');
+            break;
+        } else {
+            console.log('<<< There are no people in your contact list!');
+            break;
+        }
+    }
+}
+
+function listContact() {
+    for (let human of allContacts) {
+        console.log(human.name, human.phone);
+    }
+}
 
 function mainMenu() {
     console.log('1. Additon contact');
     console.log('2. Setting contact');
     console.log('3. Remove contact');
     console.log('4. Find contact');
-    console.log('5. Save and exit');
+    console.log('5. Print list contact');
+    console.log('6. saveAndExit');
     let choose = readlineSync.question('>>> ');
     switch (choose) {
         case '1':
             addContact();
+            mainMenu();
             break;
-        case '1':
+        case '2':
             setContact();
+            mainMenu();
             break;
-        case '1':
+        case '3':
             removeContact();
+            mainMenu();
             break;
-        case '1':
+        case '4':
             findContact();
+            mainMenu();
             break;
-        case '1':
-            saveAndExit();
+        case '5':
+            listContact();
+            mainMenu();
+            break;
+        case '6':
+            saveFile();
             break;
         default:
+            console.log('Wrong option!');
+            mainMenu();
             break;
     }
-
 }
 
 function main() {
-    // mainMenu();
     loadData();
-    console.log(allContacts);
-
+    mainMenu();
 }
 main();
